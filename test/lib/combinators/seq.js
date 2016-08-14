189ac96240d530c0548e496a1e38e5ba3aac265b
Arturas Lebedevas <arturas.lebedevas@booking.com>
2016-08-14 14:27:35 +0200

    seq combinator pass tests

--- test/lib/combinators/seq.js
+++ test/lib/combinators/seq.js
@@ -2,24 +2,34 @@
 
 const should = require('should');
 
+const string = require('../../../lib/parsers/string');
 const seq = require('../../../lib/combinators/seq');
 
 module.exports = () => {
     describe('`seq` combinator', () => {
+        const pHelloWorld = seq(string('hello'), string(' world'));
 
         it('seq(...parsers)(valid_input) => [ ...[head, tail] ]', () => {
-            const pHelloWorld = seq('hello', ' world');
             const result = pHelloWorld('hello world!');
-            const [head, tail] = result[0];
+            const head = result.map( match => match[0] ).join('');
+            const tail = result[result.length - 1][1];
 
-            should(head).equal.to('hello world');
-            should(tail).equal.to('!');
+            should(head).equal('hello world');
+            should(tail).equal('!');
 
         });
 
-        it('seq(...parsers)(invalid_input) => [ ]');
+        it('seq(...parsers)(invalid_input) => [ ]', () => {
+            const result = pHelloWorld('abc');
 
-        it('seq()(input > 0) => [ ]');
+            should(result.length).equal(0);
+        });
+
+        it('seq()(|input| > 0) => [ ]', () => {
+            const result = seq()('abc');
+
+            should(result.length).equal(0);
+        });
 
     });
 };
