7ea44a2243beb9e445d75492c87bc1d6d970f32c
Arturas Lebedevas <arturas.lebedevas@booking.com>
2016-08-14 12:11:42 +0200

    Add combinator and parser test suit

--- test/index.js
+++ test/index.js
@@ -2,6 +2,23 @@ const LIB_TESTS = [
     'memoise'
 ];
 
+const COMBINATORS_TESTS = [
+    'seq'
+];
+
+const PARSERS_TESTS = [
+    'string',
+    'regex'
+];
+
 LIB_TESTS.forEach((testName) => {
     require(`./lib/${testName}`)();
+});
+
+PARSERS_TESTS.forEach((testName) => {
+    require(`./lib/parsers/${testName}`)();
+});
+
+COMBINATORS_TESTS.forEach((testName) => {
+    require(`./lib/combinators/${testName}`)();
 });
